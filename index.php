d8423a2718399b43380206405aa0494024e2f830
phigino <phigino@gmail.com>
2018-01-31 02:24:05 -0200

    index
    Criado arquivo index para o APP, onde vai ser possível listar as mortes por jogador.

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Teste</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js" integrity="sha384-feJI7QwhOS+hwpX2zkaeJQjeiwlhOP+SdQDqhgvvo1DsjtiSQByFdThsxO669S2D" crossorigin="anonymous"></script>
+        <style>
+            body{
+                background-color: #dadada;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+
+                    <label class="text-uppercase"><strong>Ranking</strong></label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-8"> 
+                    <input type="text" name="busca" class="form-control" id="nome" placeholder="Busca por nome">
+
+                </div>
+                <div class="col-4"><button type="submit" class="btn btn-primary">Buscar</button></div>
+
+            </div>
+
+
+
+        </div>
+
+    </body>
+</html>
